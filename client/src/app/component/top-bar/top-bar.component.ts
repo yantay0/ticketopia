57d51cb3ef29898092f2d2fe9b03fc839c917ca1
isolent <a_kakharmanova@kbtu.kz>
2023-04-08 12:26:31 +0600

    logo and account img added

--- client/src/app/component/top-bar/top-bar.component.ts
+++ client/src/app/component/top-bar/top-bar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent {
-
+export class TopBarComponent implements OnInit{
+  ngOnInit(): void {
+  }
 }
