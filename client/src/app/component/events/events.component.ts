6e9250a23fbeb48cf7de61a8b9fc510581f27329
yantay0 <madinayantai2464@gmail.com>
2023-04-09 00:00:31 +0600

    fix events category from link to button

--- client/src/app/component/events/events.component.ts
+++ client/src/app/component/events/events.component.ts
@@ -1,16 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import {categories} from "../../TempDB";
+import {events} from "../../TempDB";
+import {Category} from "../../model/Category";
+
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent implements OnInit{
+export class EventsComponent implements OnInit {
   categories = categories;
-  filteredCategories = [];
+
+  filteredCategories = events;
+  allEvents = events;
+  selectedCategory: String;
+
   ngOnInit(): void {
 
+  }
+
+  constructor() {
+    this.selectedCategory = "Concert";
+  }
+
+
+  displayEventsByCategory(){
+
+  }
+
+  selectCategory(category: Category) {
+    this.categories.forEach(c => c.selected = false)
+    category.selected = true;
   }
 
 }
