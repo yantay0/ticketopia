f77655ff34d49eb7394d4cbe6b8111c1b458595d
yantay0 <madinayantai2464@gmail.com>
2023-04-09 11:44:50 +0600

    fix: update storage so interfaces are assignbale

--- client/src/app/TempDB.ts
+++ client/src/app/TempDB.ts
@@ -4,27 +4,24 @@ export const categories =  [
   {
     "id" : 1,
     "name": "Concerts",
-    "image": "concert.png",
-    "selected": true
+    "image": "concert.png"
   },
   {
     "id" : 2,
     "name": "Movies",
-    "image": "movies.png",
-    "selected": false,
+    "image": "movies.png"
   },
   {
     "id" : 3,
     "name": "Theatre",
-    "image": "theatre.png",
-    "selected": false,
+    "image": "theatre.png"
   },
 
 ]
 
 export const events = [
   {
-    "id": 1,
+    "id": 3,
     "title": "I am everything",
     "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
       "\n" +
@@ -38,118 +35,53 @@ export const events = [
     "poster": "banner5.png",
     "capacity": 200,
     "category": {
-      "id" : 1,
-      "name": "Concerts",
-      "image": "concert.png",
-      "selected": true
+      "id" : 3,
+      "name": "Theatre",
+      "image": "theatre.png"
     },
     "genre": "drama"
   },
-
-//   {
-//     "id": 2,
-//     "title": "I am everything",
-//     "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
-//       "\n" +
-//       "Attend Marilyn Monroe's birthday? Or decide the fate of the whole world with President Kennedy? Together with the heroes of the performance, the viewer will plunge into the life of great artists, actors and musicians! The starting point is the \"Bar in Harlem\", where you can hear the latest gossip from the jazz scene of the last century. And the evening will end at the concert of the \"First Lady of Jazz\".\n" +
-//       "\n" +
-//       "The linear structure of the performance is based on the real story of the production project Song Books, the author of which was the famous jazz producer Norman Grantz, and the great Ella Fitzgerald was the implementer. This is a dedication to a vanishing era of intelligence and elitism in American popular music.",
-//     "premiereDate":  "2018-06-29",
-//     "ageRating": "+18",
-//     "location": "Almaty",
-//     "duration": "140",
-//     "poster": "img.png",
-//     "capacity": 200,
-//     "category": {
-//       "id" : 1,
-//       "name": "Concerts",
-//       "image": "concert.png",
-//       "selected": true
-//     }
-//   },
-//   {
-//     "id": 3,
-//     "title": "I am everything",
-//     "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
-//       "\n" +
-//       "Attend Marilyn Monroe's birthday? Or decide the fate of the whole world with President Kennedy? Together with the heroes of the performance, the viewer will plunge into the life of great artists, actors and musicians! The starting point is the \"Bar in Harlem\", where you can hear the latest gossip from the jazz scene of the last century. And the evening will end at the concert of the \"First Lady of Jazz\".\n" +
-//       "\n" +
-//       "The linear structure of the performance is based on the real story of the production project Song Books, the author of which was the famous jazz producer Norman Grantz, and the great Ella Fitzgerald was the implementer. This is a dedication to a vanishing era of intelligence and elitism in American popular music.",
-//     "premiereDate":  "2018-06-29",
-//     "ageRating": "+18",
-//     "location": "Almaty",
-//     "duration": "140",
-//     "poster": "img.png",
-//     "capacity": 200,
-//     "category": {
-//       "id" : 1,
-//       "name": "Concerts",
-//       "image": "concert.png",
-//       "selected": true
-//     }
-//   },
-//   {
-//     "id": 4,
-//     "title": "I am everything",
-//     "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
-//       "\n" +
-//       "Attend Marilyn Monroe's birthday? Or decide the fate of the whole world with President Kennedy? Together with the heroes of the performance, the viewer will plunge into the life of great artists, actors and musicians! The starting point is the \"Bar in Harlem\", where you can hear the latest gossip from the jazz scene of the last century. And the evening will end at the concert of the \"First Lady of Jazz\".\n" +
-//       "\n" +
-//       "The linear structure of the performance is based on the real story of the production project Song Books, the author of which was the famous jazz producer Norman Grantz, and the great Ella Fitzgerald was the implementer. This is a dedication to a vanishing era of intelligence and elitism in American popular music.",
-//     "premiereDate":  "2018-06-29",
-//     "ageRating": "+18",
-//     "location": "Almaty",
-//     "duration": "140",
-//     "poster": "img.png",
-//     "capacity": 200,
-//     "category": {
-//       "id" : 1,
-//       "name": "Concerts",
-//       "image": "concert.png",
-//       "selected": true
-//     }
-//   },
-//   {
-//     "id": 5,
-//     "title": "I am everything",
-//     "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
-//       "\n" +
-//       "Attend Marilyn Monroe's birthday? Or decide the fate of the whole world with President Kennedy? Together with the heroes of the performance, the viewer will plunge into the life of great artists, actors and musicians! The starting point is the \"Bar in Harlem\", where you can hear the latest gossip from the jazz scene of the last century. And the evening will end at the concert of the \"First Lady of Jazz\".\n" +
-//       "\n" +
-//       "The linear structure of the performance is based on the real story of the production project Song Books, the author of which was the famous jazz producer Norman Grantz, and the great Ella Fitzgerald was the implementer. This is a dedication to a vanishing era of intelligence and elitism in American popular music.",
-//     "premiereDate":  "2018-06-29",
-//     "ageRating": "+18",
-//     "location": "Almaty",
-//     "duration": "140",
-//     "poster": "img.png",
-//     "capacity": 200,
-//     "category": {
-//       "id" : 1,
-//       "name": "Concerts",
-//       "image": "concert.png",
-//       "selected": true
-//     }
-//   },
-//   {
-//     "id": 6,
-//     "title": "I am everything",
-//     "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
-//       "\n" +
-//       "Attend Marilyn Monroe's birthday? Or decide the fate of the whole world with President Kennedy? Together with the heroes of the performance, the viewer will plunge into the life of great artists, actors and musicians! The starting point is the \"Bar in Harlem\", where you can hear the latest gossip from the jazz scene of the last century. And the evening will end at the concert of the \"First Lady of Jazz\".\n" +
-//       "\n" +
-//       "The linear structure of the performance is based on the real story of the production project Song Books, the author of which was the famous jazz producer Norman Grantz, and the great Ella Fitzgerald was the implementer. This is a dedication to a vanishing era of intelligence and elitism in American popular music.",
-//     "premiereDate":  "2018-06-29",
-//     "ageRating": "+18",
-//     "location": "Almaty",
-//     "duration": "140",
-//     "poster": "img.png",
-//     "capacity": 200,
-//     "category": {
-//       "id" : 1,
-//       "name": "Concerts",
-//       "image": "concert.png",
-//       "selected": true
-//     }
-//   }
+  {
+    "id": 2,
+    "title": "Marvel",
+    "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
+      "\n" +
+      "Attend Marilyn Monroe's birthday? Or decide the fate of the whole world with President Kennedy? Together with the heroes of the performance, the viewer will plunge into the life of great artists, actors and musicians! The starting point is the \"Bar in Harlem\", where you can hear the latest gossip from the jazz scene of the last century. And the evening will end at the concert of the \"First Lady of Jazz\".\n" +
+      "\n" +
+      "The linear structure of the performance is based on the real story of the production project Song Books, the author of which was the famous jazz producer Norman Grantz, and the great Ella Fitzgerald was the implementer. This is a dedication to a vanishing era of intelligence and elitism in American popular music.",
+    "premiereDate":  "2018-06-29",
+    "ageRating": "+18",
+    "location": "Almaty",
+    "duration": 140,
+    "poster": "banner3.png",
+    "capacity": 200,
+    "category": {
+      "id" : 2,
+      "name": "Movies",
+      "image": "Movies.png",
+    },
+    "genre": "fantasy"
+  },
+  {
+    "id": 3,
+    "title": "Music Festival",
+    "description": "On May 12-13-14, in the Almaty Central Department Store, STAGES together with ARARAT presents the premiere of the immersive performance \"Ideal Past\"!\n" +
+      "\n" +
+      "Attend Marilyn Monroe's birthday? Or decide the fate of the whole world with President Kennedy? Together with the heroes of the performance, the viewer will plunge into the life of great artists, actors and musicians! The starting point is the \"Bar in Harlem\", where you can hear the latest gossip from the jazz scene of the last century. And the evening will end at the concert of the \"First Lady of Jazz\".\n" +
+      "\n" +
+      "The linear structure of the performance is based on the real story of the production project Song Books, the author of which was the famous jazz producer Norman Grantz, and the great Ella Fitzgerald was the implementer. This is a dedication to a vanishing era of intelligence and elitism in American popular music.",
+    "premiereDate":  "2018-06-29",
+    "ageRating": "+18",
+    "location": "Almaty",
+    "duration": 140,
+    "poster": "banner4.png",
+    "capacity": 200,
+    "category": {
+      "id" : 1,
+      "name": "Concerts",
+      "image": "concerts.png"
+    },
+    "genre": "other"
+  },
 ]
 
