c7fcc587260540edf4b4306a450151b3c01a9b3c
yantay0 <madinayantai2464@gmail.com>
2023-04-13 14:44:06 +0600

    feat: add two get api's for categories and events list

--- ticketopia-backend/ticketopia/event/views.py
+++ ticketopia-backend/ticketopia/event/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from event.models import Category, Event
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    categories_json = [c.to_json() for c in categories]
+    return JsonResponse(categories_json, safe=False)
+
+
+def event_list(request):
+    events = Event.objects.all()
+    events_json = [e.to_json() for e in events]
+    return JsonResponse(events_json, safe=False)
+
+
