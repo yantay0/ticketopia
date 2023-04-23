f76b66a0c990ac7cf51021161adc49f7c5a5c309
yantay0 <madinayantai2464@gmail.com>
2023-04-23 16:55:23 +0600

    update models

---
+++ ticketopia-backend/ticketopia/api/serializers.py
@@ -0,0 +1,43 @@
+from event.models import *
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+
+    def create(self, validated_data):
+        category = Category.objects.create(**validated_data)
+        return category
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+class EventSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+        read_only_fields = ['id', 'category']
+
+
+class LocationSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    city = serializers.CharField(max_length=255, default=None)
+    address = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        location = Location.objects.create(**validated_data)
+        return location
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.city = validated_data.get('city', instance.city)
+        instance.address = validated_data.get('address', instance.address)
+        instance.save()
+        return instance
--- ticketopia-backend/ticketopia/event/views/fbv.py
+++
@@ -1,4 +0,0 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-
-
