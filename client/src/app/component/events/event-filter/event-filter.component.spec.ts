a377fd34fc6b579e931a83ce342542d7f5f9e3fd
yantay0 <madinayantai2464@gmail.com>
2023-04-26 01:00:57 +0600

    feat: add event-filter component for selecting category, filter events by categories; show all events by default

---
+++ client/src/app/component/events/event-filter/event-filter.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventFilterComponent } from './event-filter.component';
+
+describe('EventFilterComponent', () => {
+  let component: EventFilterComponent;
+  let fixture: ComponentFixture<EventFilterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EventFilterComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EventFilterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- client/src/app/component/events/events.component.ts
+++ client/src/app/component/events/events.component.ts
@@ -1,35 +1,41 @@
-import {Component, OnInit} from '@angular/core';
-// import {categories} from "../../TempDB";
-// import {events} from "../../TempDB";
-import {Category} from "../../model/Category";
-import {Router} from "@angular/router";
-import {DOMAIN} from "../../config";
-import {CategoryService} from "../../service/category.service";
+import { Component, OnInit } from '@angular/core';
+import { Category } from "../../model/Category";
+import { ActivatedRoute } from "@angular/router";
+import { EventService } from "../../service/event.service";
+import { Event } from "../../model/Event";
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent implements OnInit{
+export class EventsComponent implements OnInit {
 
-  categories : Category[] = [];
-  selectedCategory: Category;
+  events: Event[] = [];
+  category: Category;
+  filteredEvents: Event[] = [];
+
+  constructor(private route: ActivatedRoute, private eventService: EventService) {
+    this.category = {} as Category
+  }
+
   ngOnInit(): void {
-    this.categoryService.getCategories().subscribe((data: Category[])=>{
-      this.categories = data;
-      this.selectedCategory = data[0];
-    })
+    this.route.paramMap.subscribe(params => {
+      this.category.name = <string>params.get('category');
+      this.allEvents();
+    });
   }
 
-  constructor(private router: Router, private categoryService: CategoryService) {
-    this.selectedCategory = {} as Category;
-  }
-  onSelect(category: Category) {
-    this.selectedCategory = category;
-    console.log(this.selectedCategory)
-    if(category.id == 1) this.router.navigate([DOMAIN]);
-    else this.router.navigate([`${DOMAIN}/${category.name.toLowerCase()}`])
+  private allEvents() {
+    this.eventService.getAllEvents().subscribe(events => {
+      this.events = events;
+      this.applyFilter();
+    });
   }
 
+  private applyFilter() {
+    this.filteredEvents = this.events.filter(e => e.category.name.toLowerCase() === this.category.name);
+    console.log(this.filteredEvents);
+    console.log(this.category)
+  }
 }
