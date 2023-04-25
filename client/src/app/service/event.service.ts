c03a9c5301b510726ec66f8cff6e28856ed7f2aa
yantay0 <madinayantai2464@gmail.com>
2023-04-26 00:58:45 +0600

    feat: add event service gitAllEvents function

---
+++ client/src/app/service/event.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Event} from "../model/Event";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+  BASE_URL = 'http://127.0.0.1:8000'
+  constructor(private client: HttpClient) { }
+
+  getAllEvents():Observable<Event[]>{
+    return this.client.get<Event[]>(
+      `${this.BASE_URL}/api/events`
+    )
+  }
+}
