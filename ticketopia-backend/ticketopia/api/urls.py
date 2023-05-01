41b17c7e9bcf135b17185733208ecd1c02ca565c
yantay0 <madinayantai2464@gmail.com>
2023-05-01 20:23:39 +0600

    fix: change login api as api/user

--- ticketopia-backend/ticketopia/api/urls.py
+++ ticketopia-backend/ticketopia/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from api import views
-from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path('login/', obtain_jwt_token),
     path('categories/', views.CategoryListApiView.as_view()),
     path('events/', views.event_list),
     path('categories/<int:category_id>/events/', views.event_list_by_category),
--- ticketopia-backend/ticketopia/user/api/urls.py
+++ ticketopia-backend/ticketopia/user/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
+
 from user.api import views
 
 app_name = "user"
 
 urlpatterns = [
-    path('register/', views.registration_view)
+    path('register/', views.registration_view),
+    path('login/', obtain_jwt_token),
 ]
