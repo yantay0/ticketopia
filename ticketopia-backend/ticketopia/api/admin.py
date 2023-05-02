4a61012411bf7ee156f41bd0833bb285cab81988
yantay0 <madinayantai2464@gmail.com>
2023-05-02 21:17:43 +0600

    feat: add account model, update ticket, event, location model/ add additional fields

--- ticketopia-backend/ticketopia/api/admin.py
+++ ticketopia-backend/ticketopia/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import Location, Event, Category
+from api.models import Location, Event, Category, Ticket, Account
 
 
 # from ticket.models import Ticket
@@ -12,14 +12,24 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'location', 'category', 'quantity')
+    list_display = ('id', 'name', 'description', 'premiere_date',
+                    'age_rating', 'location', 'duration', 'poster',
+                    'category', 'tickets_quantity_dance_floor',
+                    'tickets_quantity_vip_zone', 'tickets_quantity_seating_area')
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'city', 'address')
+    list_display = ('id', 'name', 'city', 'address', 'has_seating_area', 'has_dance_floor',
+                    'has_vip_zone', 'dance_floor_capacity',
+                    'seating_area_capacity', 'seating_area_rows')
 
-# #
-# @admin.register(Ticket)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'event', 'row', 'seat', 'price', 'ticket_type')
+
+@admin.register(Ticket)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'event', 'row', 'seat', 'price', 'ticket_type')
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'last_name', 'first_name', 'phone_number')
--- ticketopia-backend/ticketopia/api/models.py
+++ ticketopia-backend/ticketopia/api/models.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils.translation import ugettext_lazy as _
 
 User = get_user_model()
 
@@ -35,6 +33,12 @@ class Location(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255, default=None)
     address = models.CharField(max_length=255)
+    has_seating_area = models.BooleanField(default=False)
+    has_dance_floor = models.BooleanField(default=False)
+    has_vip_zone = models.BooleanField(default=False)
+    dance_floor_capacity = models.IntegerField(null=True, blank=True)
+    seating_area_capacity = models.IntegerField(null=True, blank=True)
+    seating_area_rows = models.IntegerField(null=True, blank=True)
 
     class Meta:
         verbose_name = 'Location'
@@ -52,13 +56,18 @@ class Event(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
     duration = models.IntegerField(default=0)
     poster = models.CharField(max_length=255, null=True, default="soon.png", verbose_name='Poster URL')
-    quantity = models.IntegerField(default=0)
+    tickets_quantity_dance_floor = models.IntegerField(default=0)
+    tickets_quantity_vip_zone = models.IntegerField(default=0)
+    tickets_quantity_seating_area = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="events")
     genre = models.CharField(max_length=255, null=True)
 
     class Meta:
         verbose_name = 'Event'
         verbose_name_plural = 'Events'
+
+    def total_tickets_quantity(self):
+        return self.tickets_quantity_dance_floor + self.tickets_quantity_vip_zone + self.tickets_quantity_seating_area
 
     def __str__(self):
         return f'{self.id} : {self.name} : {self.category}: {self.genre}'
@@ -69,7 +78,12 @@ class Ticket(models.Model):
     row = models.CharField(max_length=10)
     seat = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    ticket_type = models.CharField(max_length=255)
+    TICKET_TYPE = [
+        ('dance_floor', 'Dance Floor'),
+        ('vip', 'VIP'),
+        ('seating', 'Seating Area')
+    ]
+    ticket_type = models.CharField(max_length=255, choices=TICKET_TYPE)
 
     class Meta:
         verbose_name = 'Ticket'
@@ -77,3 +91,13 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f'{self.id} : {self.event} :  {self.row} : {self.seat} :{self.price} : {self.ticket_type}'
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    tickets = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='users', null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
