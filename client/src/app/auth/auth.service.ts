42016d48a6179735eaf71407e19d0120334103ca
yantay0 <madinayantai2464@gmail.com>
2023-05-02 17:04:58 +0600

    feat: add check 400 error (wrong email or password) in login

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {AuthToken} from "../model/auth-token";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  BASE_URL = 'http://127.0.0.1:8000'
+  constructor(private client: HttpClient) { }
+
+
+  login(email: string, password: string): Observable<AuthToken> {
+    return this.client.post<AuthToken>(
+      `${this.BASE_URL}/api/user/login/`,
+      {email, password}
+    )
+  };
+
+
 }
--- client/src/app/component/login/login.component.ts
+++ client/src/app/component/login/login.component.ts
@@ -2,6 +2,9 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {CategoryService} from "../../service/category.service";
 import {Router} from "@angular/router";
 import {DOMAIN} from "../../config";
+import {AuthService} from "../../auth/auth.service";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+import {AuthToken} from "../../model/auth-token";
 
 @Component({
   selector: 'app-login',
@@ -12,25 +15,45 @@ export class LoginComponent implements OnInit{
   email: string = ''
   password: string = ''
   @Output() isLogged: EventEmitter<boolean> = new EventEmitter<boolean>();
-  constructor(private categoryService: CategoryService, private router: Router) {}
+  errorMessage: string | undefined;
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     const token = localStorage.getItem('token')
     if(token) {
       this.isLogged.emit(true)
     }
-
   }
+
+
 
   login() {
-    this.categoryService.login(this.email, this.password).subscribe((data) =>{
-      localStorage.setItem('token', data.token);
-      this.router.navigate([`${DOMAIN}`])
-      this.isLogged.emit(true);
-      this.isLogged.emit(false);
-      this.email = ''
-      this.password = ''
-    });
+    this.errorMessage = ''; // Initialize error message to empty string
+
+    try {
+      this.authService.login(this.email, this.password).subscribe((data) =>{
+        localStorage.setItem('token', data.token);
+        console.log(data)
+        this.router.navigate([`${DOMAIN}`])
+        this.isLogged.emit(true);
+        this.isLogged.emit(false);
+        this.email = ''
+        this.password = ''
+      }, (error) => {
+        // Handle login error
+        if (error.status === 400) {
+          this.errorMessage = 'Email or password is incorrect';
+        } else {
+          this.errorMessage = 'An error occurred, please try again later';
+        }
+      });
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
   }
+
+
+
+
 
 }
